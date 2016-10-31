012dc23ae36c35754d6980cee441bb17fcf9c92b
LadyVamp <ladyvamp666@gmail.com>
2016-11-01 00:41:08 +0200

    1.9 full

--- js/graphs.js
+++ js/graphs.js
@@ -490,3 +490,249 @@ $(function () {
         });
     });
 });
+
+//стр. 104
+//Консолидированный бюджет АФК «Система» на благотворительность
+$(function () {
+
+    $(document).ready(function () {
+
+        // Build the chart
+        $('#graphP104').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                // pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                pointFormat: '{series.name} <b>{point.y}/3 </b>'
+
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: ' ',
+                colorByPoint: true,
+                data: [{
+                    name: 'Общекорпоративная программа БФ «Система»',
+                    y: 2
+                }, {
+                    name: 'Самостоятельные благотворительные проекты ДЗК',
+                    y: 1,
+                    sliced: true,
+                    selected: true
+                }, 
+                // {
+                //     name: 'Firefox',
+                //     y: 10.38
+                // }
+                ]
+            }]
+        });
+    });
+});
+
+//стр 105
+//Структура финансирования проектов БФ «Система» в 2015 году, %
+$(function () {
+
+    $(document).ready(function () {
+
+        // Build the chart
+        $('#graphP105Left').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                // pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                pointFormat: '{series.name} <b>{point.y} %</b>'
+
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: ' ',
+                colorByPoint: true,
+                data: [{
+                    name: 'Наука. Образование. Просвещение',
+                    y: 49
+                }, {
+                    name: 'Культура и искусство',
+                    y: 8,
+                    sliced: true,
+                    selected: true
+                }, 
+                 {
+                     name: 'Сохранение и развитие духовного наследия',
+                     y: 15
+                 },
+                 {
+                     name: 'Охрана здоровья и социальная поддержка',
+                     y: 23
+                 },
+                 {
+                     name: 'Физкультура и спорт',
+                     y: 3
+                 },
+                 {
+                     name: 'Расходы на организацию мероприятий корпоративного волонтерского центра',
+                     y: 2
+                 }
+                ]
+            }]
+        });
+    });
+});
+
+//стр 105
+//Общие инвестиции в благотворительные и социальные программы в разбивке по ДЗК, %
+$(function () {
+
+    $(document).ready(function () {
+
+        // Build the chart
+        $('#graphP105Right').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                // pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                pointFormat: '{series.name} <b>{point.y} %</b>'
+
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: ' ',
+                colorByPoint: true,
+                data: [{
+                    name: 'МТС',
+                    y: 75
+                }, {
+                    name: '"Таргин"',
+                    y: 10,
+                    sliced: true,
+                    selected: true
+                }, 
+                 {
+                     name: 'РТИ',
+                     y: 6
+                 },
+                 {
+                     name: 'Segezha Group',
+                     y: 3
+                 },
+                 {
+                     name: 'БЭСК',
+                     y: 1
+                 },
+                 {
+                     name: 'Прочие',
+                     y: 5
+                 }
+                ]
+            }]
+        });
+    });
+});
+
+//стр. 105
+//Структура расходов ДЗК АФК «Система» на благотворительные и социальные программы в 2015 году, % 
+$(function () {
+    // Create the chart
+    $('#graphP105Low').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+            // text: ' '
+        },
+        xAxis: {
+            type: 'category'
+        },
+        yAxis: {
+            title: {
+                // text: 'Total percent market share'
+                text: ' '
+            }
+
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.y:.1f}%'
+                }
+            }
+        },
+
+        tooltip: {
+           pointFormat: '{series.name} <b>{point.y} %</b>'
+        },
+
+        series: [{
+            // name: 'Выручка, млрд. руб',
+            name: ' ',
+            colorByPoint: true,
+            data: [{
+                name: 'Внешние КСО и благотворительные программы',
+                // label: '% от ВВП',
+                // profit: 646.3,
+                y: 60.3
+            }, {
+                name: 'Внутренние социальные программы',
+                // label: '% от ВВП',
+                // profit: 708.6,  
+                y:  39.7
+             
+            }]
+        }],
+
+    });
+});
